7c58b91d8e714924b193f24cfa74e3e5ab43cc16
eungyolee <vlstpt812@gmail.com>
2025-05-28 16:32:13 +0900

    250528 7교시

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from .router import router as api_router
 from .utils.exceptions import ValidationException, LoginFailedException
 from .middleware import middlewares
 
 app = FastAPI(
 	title="AnA FastAPI 실습",
-	description="아 하기 싫다 집가고 싶다",
+	description="노현우 죽인다",
 	version="1.0.0"
 )
 
 for middleware in middlewares:
 	app.middleware("http")(middleware)
+
+origins = [
+	'http://localhost:3000',
+	'https://naver.com',
+	'https://eungyolee.kr'
+]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,  # 허용할 출처 -> origins
+	allow_credentials=True,  # 자격 증명 허용 여부 -> 참
+	allow_methods=["*"],  # 허용할 메서드 -> 전부
+	allow_headers=["*"]  # 허용할 헤더 -> 전부
+)
 
 app.include_router(api_router)
 
--- backend/router/__init__.py
+++ backend/router/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from .password_router import router as password_router
 from .student_router import router as student_router
 from .file_router import router as file_router
+from .ping_router import router as ping_router
 
 router = APIRouter()
 
 router.include_router(password_router, tags=["250516"])
 router.include_router(student_router, tags=["250516"])
 router.include_router(file_router, tags=["250523"])
+router.include_router(ping_router, tags=["250528"])
